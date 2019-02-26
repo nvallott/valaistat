889b5f1f547c486442ff023cbab0c84e4e8b22c6
nvallott <nvallott@hotmail.com>
2019-02-26 14:31:07 +0100

    transfer valaistat project

---
+++ static/lib/tooltips.js
@@ -0,0 +1,18 @@
+var labels =
+[
+  {indic : "/d0t0", label: "Densité de population"},
+  {indic : "/d0t1", label: "Population totale"},
+  {indic : "/d0t2", label: "% d'étrangers"},
+  {indic : "/d0t3", label: "% de francophones"},
+  {indic : "/d0t4", label: "% de germanophones"},
+  {indic : "/d0t5", label: "Nombre d'anglophones"},
+  {indic : "/d0t6", label: "% de jeunes"},
+  {indic : "/d0t7", label: "% de personnes âgées"},
+  {indic : "/d1t0", label: "Logements totaux"},
+  {indic : "/d2t0", label: "% de pendulaires sortants"},
+  {indic : "/d3t0", label: "% votes étrangers criminels"},
+  {indic : "/d4t0", label: "% scolarité obligatoire"},
+  {indic : "/d4t1", label: "% diplomés haute école"},
+  {indic : "/d5t0", label: "% de chômeurs"},
+  {indic : "/d6t0", label: "District"}
+];
